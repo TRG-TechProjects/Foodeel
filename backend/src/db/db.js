bf67e288f9b127defb419f041874f65b8df82557
Devmalya Ray <devmalyaray@gmail.com>
2026-03-16 02:15:21 +0530

    Fix MongoDB connection

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,14 +1,31 @@
-// database connection
+// // database connection
+// const mongoose = require('mongoose');
+
+// function connectDB() {
+//     mongoose.connect(process.env.MONGODB_URI)
+//     .then(() => {
+//         console.log("MongoDB connected");
+//     })
+//     .catch((err) => {
+//         console.log("MongoDB connection error:", err);
+//     })
+// }
+
+// module.exports = connectDB;
+
 const mongoose = require('mongoose');
 
-function connectDB() {
-    mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
         console.log("MongoDB connected");
-    })
-    .catch((err) => {
-        console.log("MongoDB connection error:", err);
-    })
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+    }
 }
 
 module.exports = connectDB;
